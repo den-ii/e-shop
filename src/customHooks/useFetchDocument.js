c336fb582cece3e9fcda6be7d7439aff40bf7b58
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-03-28 09:05:31 +0100

    fix flex-start

--- src/customHooks/useFetchDocument.js
+++ src/customHooks/useFetchDocument.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
